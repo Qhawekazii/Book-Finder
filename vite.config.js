d6e372ae83cea45e4d454dd58c19efef9c79a8f9
Emily Maramani <emilyqm01@gmail.com>
2026-07-22 12:14:53 +0200

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Book-Finder/',
+  base: '/book-finder-api-app/',
   plugins: [react()],
 });
